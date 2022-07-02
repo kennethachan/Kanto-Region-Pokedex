d0e4b9607f507f7112432b13848b5c6398d192aa
kennethachan <kennethandrechan@gmail.com>
2022-07-02 00:24:52 -0400

    random pokemon generator is running w speific stats displaying

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const pokemonCard = document.getElementById("pokemon-card")
 const randomBtn = document.getElementById("random")
 
-const pokeImage = document.getElementById("poke-image")
-const pokemonName = document.getElementsByClassName("poke-name")
-const hp = document.getElementById("hp")
-const id = document.getElementById("id")
-const attackNumber = document.getElementById("attack-number")
-const defenseNumber = document.getElementById("defense-number")
-const speedNumber = document.getElementById("speed-number")
-const typeOne = document.getElementById("type-one")
-const typeTwo = document.getElementById("type-two")
+// const pokeImage = document.getElementById("poke-image")
+// const pokemonName = document.getElementsByClassName("poke-name")
+// const hp = document.getElementById("hp")
+// const id = document.getElementById("id")
+// const attackNumber = document.getElementById("attack-number")
+// const defenseNumber = document.getElementById("defense-number")
+// const speedNumber = document.getElementById("speed-number")
+// const typeOne = document.getElementById("type-one")
+// const typeTwo = document.getElementById("type-two")
 
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/`
 
@@ -35,17 +35,38 @@ const pokeStats = () => {
 const createCard = (data) => {
   console.log(data)
   const renderName = data.name
-  const id = data.id
+  const renderId = data.id
   const renderImage = data.sprites.other.dream_world.front_default
-  //   const hp = data.stats[0].base_stat
-  //   const attackNumber = data.stats[1].base_stat
-  //   const defenseNumber = data.stats[2].base_stat
-  //   const speedNumber = data.stats[5].base_stat
-  //   const typeOne = data.types[0]
-  //   const typeTwo = data.types[1]
+  const renderHP = data.stats[0].base_stat
+  const renderAttack = data.stats[1].base_stat
+  const renderDefense = data.stats[2].base_stat
+  const renderSpeed = data.stats[5].base_stat
+  const typeOne = data.types[0].type.name
 
-  pokemonCard.innerHTML = `<img id="poke-image" src="${renderImage}"/>
-  <div class="poke-name">${renderName}</div>`
+  pokemonCard.innerHTML = `<div id="pokemon-card">
+  <img id="poke-image" src="${renderImage}"/>
+  <div class="poke-name">${renderName}</div>
+   <div id="id"><span>#</span>${renderId}</div> 
+  <div id="hp">${renderHP}</div>
+  <div id="hp-num">HP</div>  
+  <div id="type-one"><span>type: </span>${typeOne}</div>
+  <div class="stats">
+      <div> 
+          <h4 id="attack-number">${renderAttack}</h4>
+          <p id="attack"> attack</p>
+      </div>
+      <div> 
+          <h4 id="defense-number">${renderDefense}</h4>
+          <p id="defense"> defense</p>
+      </div>
+      <div> 
+          <h4 id="speed-number">${renderSpeed}</h4>
+          <p id="speed"> speed</p>
+      </div>
+  </div>
+</div>
+</div>
+  `
 }
 
 // const movieItem = document.createElement("li")
